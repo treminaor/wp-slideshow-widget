7870bf693f0dfbeb2feba6255e35b929c847d76e
Andrew King <1andyking1@gmail.com>
2019-11-05 00:36:56 -0800

    instance title output adde to frontend

--- includes/widget.php
+++ includes/widget.php
@@ -1,13 +1,15 @@
-
-<ul id="out-of-the-box-demo">
-  <?php
-    for($i = 0; $i < count($instance['slideshow']); $i++) {
-      echo '<li><a href="#slide' . $i . '">';
-      echo '<img src="' . wp_get_attachment_url($instance['slideshow'][$i]) . '">';
-      echo '</a></li>';
-    }
-  ?>
-</ul>
+<div class="widget">
+  <?php echo isset($instance['title']) ? ('<h2 class="widgettitle">' . $instance['title'] . '</h2>') : ''; ?>
+  <ul id="out-of-the-box-demo">
+    <?php
+      for($i = 0; $i < count($instance['slideshow']); $i++) {
+        echo '<li><a href="#slide' . $i . '">';
+        echo '<img src="' . wp_get_attachment_url($instance['slideshow'][$i]) . '">';
+        echo '</a></li>';
+      }
+    ?>
+  </ul>
+</div>
 
 <script>
   jQuery('#out-of-the-box-demo').slippry();
